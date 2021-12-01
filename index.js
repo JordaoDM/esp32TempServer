61ae48fe0f13f2cc690b486951fe83a1fdb31745
JordaoDM <jordaow8@live.com>
2021-12-01 11:35:46 -0300

    Modified absolute path name to the static folder

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const port = 5000;
 
 app.get("/temp", tempHandler);
 
+var absolutePathStatic = __dirname + "/static"; //define absolute path to the static folder
+app.use("/static", express.static(absolutePathStatic)); //serves static files
+
+var absolutePathIndex = __dirname + "/views/index.html"; //define absolute path to the index.html file
 function tempHandler(req, res){
     request("http://192.168.31.27/temp", function(err, body){
-        res.json(body.body);
+        //res.json(body.body);
         console.log(JSON.stringify(body.body));
+        res.sendFile(absolutePathIndex)
     });
 }
 
