a60eb48c9c452049d2aa2ad002ed23819e4f327e
JordaoDM <jordaow8@live.com>
2021-12-01 11:50:04 -0300

    modified endpoints

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ var request = require('request');
 const app = express();
 const port = 5000;
 
-app.get("/temp", tempHandler);
+app.get("/", mainHandler);
+app.get("/tmep", tempHandler);
 
 var absolutePathStatic = __dirname + "/static"; //define absolute path to the static folder
 app.use("/static", express.static(absolutePathStatic)); //serves static files
 
 var absolutePathIndex = __dirname + "/views/index.html"; //define absolute path to the index.html file
+function mainHandler(req, res){
+    res.sendFile(absolutePathIndex)
+}
+
 function tempHandler(req, res){
     request("http://192.168.31.27/temp", function(err, body){
-        //res.json(body.body);
+        res.json(body.body);
         console.log(JSON.stringify(body.body));
-        res.sendFile(absolutePathIndex)
     });
 }
 
